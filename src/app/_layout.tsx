e89216a4849cdaf367ef09a9f585390e0e14eb28
Hysteria <danil2899@mail.ru>
2026-08-10 20:32:51 +0700

    fix: fixed android run issues

---
+++ src/app/_layout.tsx
@@ -0,0 +1,29 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { I18nextProvider } from 'react-i18next';
+
+import i18n from '@shared/config/locales/i18n';
+import { ThemeProvider } from '@shared/theme/themeProvider';
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={styles.root}>
+      <SafeAreaProvider>
+        <I18nextProvider i18n={i18n}>
+          <ThemeProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+          </ThemeProvider>
+        </I18nextProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
